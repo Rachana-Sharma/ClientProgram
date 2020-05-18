794256022006e9e823bc3af258a36d0b21edbe89
Rachana-Sharma <45629361+Rachana-Sharma@users.noreply.github.com>
2020-05-18 18:41:33 +0530

    Add files via upload

---
+++ model/model/src/main/java/com/service/model/NumberModel.java
@@ -0,0 +1,51 @@
+package com.service.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NumberModel {
+
+	private int number1;
+	private int number2;
+	/**
+	 * @param number1
+	 * @param number2
+	 */
+	public NumberModel(int number1, int number2) {
+		super();
+		this.number1 = number1;
+		this.number2 = number2;
+	}
+	/**
+	 * 
+	 */
+	public NumberModel() {
+		
+	}
+	/**
+	 * @return the number1
+	 */
+	public int getNumber1() {
+		return number1;
+	}
+	/**
+	 * @param number1 the number1 to set
+	 */
+	public void setNumber1(int number1) {
+		this.number1 = number1;
+	}
+	/**
+	 * @return the number2
+	 */
+	public int getNumber2() {
+		return number2;
+	}
+	/**
+	 * @param number2 the number2 to set
+	 */
+	public void setNumber2(int number2) {
+		this.number2 = number2;
+	}
+	
+	
+}
---
+++ model/model/src/main/java/com/service/model/Output.java
@@ -0,0 +1,36 @@
+package com.service.model;
+
+public class Output {
+	private int result;
+
+	/**
+	 * @param result
+	 */
+	public Output(int result) {
+		
+		this.result = result;
+	}
+
+	/**
+	 * 
+	 */
+	public Output() {
+		
+	}
+
+	/**
+	 * @return the result
+	 */
+	public int getResult() {
+		return result;
+	}
+
+	/**
+	 * @param result the result to set
+	 */
+	public void setResult(int result) {
+		this.result = result;
+	}
+	
+
+}
